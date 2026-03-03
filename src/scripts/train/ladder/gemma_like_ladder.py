68aa48cff4dc72b72b5d7593c71f4dbbf24cafe8
Yashas Samaga <yashas_2010@yahoo.com>
2026-03-03 12:40:23 -0800

    disable lnwd ablation for 1.3B ladder

--- src/scripts/train/ladder/gemma_like_ladder.py
+++ src/scripts/train/ladder/gemma_like_ladder.py
@@ -795,6 +795,7 @@ def build_experiment_config(cli_context: CliContext) -> ExperimentConfig:
             betas=(0.9, 0.95),
             group_overrides=[
                 OptimGroupOverride(params=["embeddings.weight"], opts=dict(weight_decay=0.0)),
+                OptimGroupOverride(params=["*norm.weight"], opts=dict(weight_decay=0.0))
             ],
         ),
         scheduler=CosWithWarmupAndLinearDecay(
