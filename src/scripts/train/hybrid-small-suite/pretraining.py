88831415540630b91b761fb9f8feab9dcf3bb83f
Yashas Samaga <yashas_2010@yahoo.com>
2026-06-02 21:53:10 -0700

    Cx4

--- src/scripts/train/hybrid-small-suite/pretraining.py
+++ src/scripts/train/hybrid-small-suite/pretraining.py
@@ -90,7 +90,7 @@ from olmo_core.train.train_module import (
     TransformerTrainModuleConfig,
 )
 
-CHINCHILLA_MULTIPLE = 2
+CHINCHILLA_MULTIPLE = 4
 DATA_MIX = DataMix.OLMo_mix_0925
 
 # Per-size learning rates.
